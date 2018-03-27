93b9b58096d9b9d051e03f5abe371d91a13be656
Andrew Duong <andrewduong77@yahoo.com>
2018-03-27 15:10:10 -0500

    some progress on p2, random and mod work with array

--- Program/Program 2/rand_mod.s
+++ Program/Program 2/rand_mod.s
@@ -13,12 +13,24 @@
 main:
     BL _seedrand            @ seed the random number generator with the current time
     BL _getrand             @ get the random number
+    MOV R1, R0              @ move generated random number to R1 for mod evaluation
+    MOV R2, #100            @ move a constant value to R2 for mod evaluation
+    BL _mod_unsigned        @ compute the remainder of R1 / R2
+    ADD R0, R0, #100        @ add a constant value to R0
     MOV R1, R0              @ move the result into R1 for printf
     BL _printf              @ print the random number
     BL _getrand             @ get the random number
+    MOV R1, R0              @ move generated random number to R1 for mod evaluation
+    MOV R2, #100            @ move a constant value to R2 for mod evaluation
+    BL _mod_unsigned        @ compute the remainder of R1 / R2
+    ADD R0, R0, #100        @ add 100 to R0
     MOV R1, R0              @ move the result into R1 for printf
     BL _printf              @ print the random number
     BL _getrand             @ get the random number
+    MOV R1, R0              @ move generated random number to R1 for mod evaluation
+    MOV R2, #100            @ move a constant value to R2 for mod evaluation
+    BL _mod_unsigned        @ compute the remainder of R1 / R2
+    ADD R0, R0, #100        @ add 100 to R0
     MOV R1, R0              @ move the result into R1 for printf
     BL _printf              @ print the random number
     B _exit                 @ exit if done
@@ -44,6 +56,22 @@ _exit:
     SWI 0                   @ execute syscall
     MOV R7, #1              @ terminate syscall, 1
     SWI 0                   @ execute syscall
+       
+_mod_unsigned:
+    cmp R2, R1              @ check to see if R1 >= R2
+    MOVHS R0, R1            @ swap R1 and R2 if R2 > R1
+    MOVHS R1, R2            @ swap R1 and R2 if R2 > R1
+    MOVHS R2, R0            @ swap R1 and R2 if R2 > R1
+    MOV R0, #0              @ initialize return value
+    B _modloopcheck         @ check to see if
+    _modloop:
+        ADD R0, R0, #1      @ increment R0
+        SUB R1, R1, R2      @ subtract R2 from R1
+    _modloopcheck:
+        CMP R1, R2          @ check for loop termination
+        BHS _modloop        @ continue loop if R1 >= R2
+    MOV R0, R1              @ move remainder to R0
+    MOV PC, LR              @ return
        
 _printf:
     PUSH {LR}               @ store the return address
